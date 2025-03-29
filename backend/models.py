c4f8b3ef506acfe209f636809a061db3b77d73d6
AbhishekSinghDIkhit <aabhishek576@gmail.com>
2025-03-29 14:37:31 +0530

    update

--- backend/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class PresentationRequest(BaseModel):
-    title: str
-    author: str
-    num_slides: int
-    description: str  # Can be empty if AI is used
-    useAI: bool
